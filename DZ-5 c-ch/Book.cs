a54dbdf37c88b92f8ed03a5ba49f267984cfe85a
Ura-sapfir <Ura-sapfir@HOME-PC>
2024-05-20 01:12:49 +0500

    Добавьте файлы проекта.

---
+++ DZ-5 c-ch/Book.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ_5_c_ch
+{
+    public class Book
+    {
+        public string Name {  get; set; }
+        public string Author { get; set; }
+        public int Number_of_pages { get; set; }
+        public int Age { get; set; }
+        public Book(string name, string author, int numpages, int age) { 
+            Name = name;
+            Author = author;
+            Number_of_pages = numpages;
+            Age = age;
+
+        }
+
+        public void Info()
+        {
+            Console.WriteLine($"Название: {Name}, Автор: {Author}, кол-во страниц: {Number_of_pages}, год издания книги: {Age}");
+        }
+
+        public static bool operator ==(Book left, Book right)
+        {
+            
+            left.Name = right.Name;
+            left.Author == right.Author;
+            left.Number_of_pages==right.Number_of_pages;
+            left.Age == right.Age;
+            return true;
+
+        }
+        public static bool operator !=(Book left, Book right)
+        {
+            left.Name = right.Name;
+            left.Author = right.Author;
+            left.Number_of_pages = right.Number_of_pages;
+            left.Age = right.Age;
+            return true;
+
+        }
+
+    }
+}
---
+++ DZ-5 c-ch/list_of_books.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ_5_c_ch
+{
+    public class list_of_books
+    {
+        private List<Book> books;
+        public list_of_books(int num) {
+            books = new List<Book>(num);
+        }
+
+        public void AddBook(Book book) { books.Add(book); }
+        public void RemoveBook(Book book) {
+            books.Remove(book);
+        }
+        public Book GetBook(int num)
+        {
+            books.GetEnumerator();
+            return book;
+        }
+
+    }
+}
